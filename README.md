3c2d40d7039195344a120fc643de63b12d252d2f
cquest <cquest@openstreetmap.fr>
2019-04-30 12:26:28 +0200

    on démarre

---
+++ README.md
@@ -0,0 +1,27 @@
+# API d'interrogation des zones de restriction de vol pour les drones de loisir
+
+Zones soumises à interdictions ou à restrictions pour l’usage, à titre de loisir, d’aéronefs télépilotés (ou drones), sur le territoire métropolitain.
+
+Ces données intègrent partiellement les interdictions s’appuyant sur des données publiées hors de l’AIP (Aeronautical Information Publication) et ne couvrent pas les interdictions temporaires.
+Elles sont basées sur l’arrêté « espace » du 30 mars 2017.
+
+## Source et millésime
+
+Source des données: SIA/DGAC
+
+Millésime des données: 2018-11
+
+## Informations complémentaires
+
+La représentation des zones soumises à interdictions ou à restrictions n’engage pas la responsabilité des producteurs de la donnée.
+Le contour des agglomérations est fourni à titre purement indicatif.
+Il est rappelé que le survol d'un fleuve ou d'un parc en agglomération est interdit.
+
+Consulter ces données ou une carte ne dispense pas de connaitre la réglementation, de l’appliquer avec discernement et de rester prudent en toute occasion.
+
+Plus d'informations disponibles sur https://www.ecologique-solidaire.gouv.fr/modeles-reduits-et-drones-loisir
+
+La dernière version visualisable est disponible sur https://www.geoportail.gouv.fr/donnees/restrictions-pour-drones-de-loisir
+
+
+Aucune licence n'étant clairement indiquée pour ces données, on peut considérer qu'elles relèvent du cadre général du Code des Relations entre le Public et l'Administration (CRPA) correspondant à la Licence Ouverte 2.0.
---
+++ drones_as_api.py
@@ -0,0 +1,66 @@
+#! /usr/bin/python3
+
+# modules additionnels
+import falcon
+import psycopg2
+
+class dgac_drone(object):
+    def getDrone(self, req, resp):
+        db = psycopg2.connect("")  # connexion à la base PG locale
+        cur = db.cursor()
+
+        where = ''
+
+        lat = float(req.params.get('lat', None).replace(',','.'))
+        lon = float(req.params.get('lon', None).replace(',', '.'))
+        dist=min(int(req.params.get('dist', 1000)), 10000)
+
+        limite = req.params.get('limite', None)
+        if limite:
+            where = where + cur.mogrify(" AND limite >= %s", (limite,)).decode()
+
+        if lat and lon:  # recherche géographique
+            query = """
+select json_build_object('source', 'DGAC / SIA',
+    'derniere_maj', '2018-11',
+    'type','Featurecollection',
+    'nb_features', count(d.*),
+    'features', case when count(*)=0 then array[]::json[] else array_agg(json_build_object(
+        'type','Feature',
+            'properties',json_build_object(
+                'limite_alti_m', limite,
+                'distance_m', ST_Distance(geom::geography,st_setsrid(st_makepoint(%(lon)s, %(lat)s),4326)::geography)::int,
+                'cap_deg', case when ST_Distance(geom::geography,st_setsrid(st_makepoint(%(lon)s, %(lat)s),4326)::geography)>0
+                    then degrees(ST_Azimuth(st_setsrid(st_makepoint(%(lon)s, %(lat)s),4326), ST_ClosestPoint(geom, st_setsrid(st_makepoint(%(lon)s, %(lat)s),4326))))::int
+                    else null end
+            ),
+            'geometry',st_asgeojson(geom,6,0)::json)
+            order by ST_Distance(geom::geography,st_setsrid(st_makepoint(%(lon)s, %(lat)s),4326)::geography)
+        ) end )::text
+from drones d
+where st_buffer(st_setsrid(st_makepoint(%(lon)s, %(lat)s),4326)::geography, %(dist)s)::geometry && geom
+    and ST_DWithin(st_setsrid(st_makepoint(%(lon)s, %(lat)s),4326)::geography, geom::geography,  %(dist)s)
+""" % {'lon': lon, 'lat': lat, 'dist': dist} + where
+
+            print(query)
+            cur.execute(query)
+            drones = cur.fetchone()
+
+            resp.status = falcon.HTTP_200
+            resp.set_header('X-Powered-By', 'drone_as_api (https://github.com/cquest/drone_as_api/)')
+            resp.set_header('Access-Control-Allow-Origin', '*')
+            resp.set_header("Access-Control-Expose-Headers","Access-Control-Allow-Origin")
+            resp.set_header('Access-Control-Allow-Headers','Origin, X-Requested-With, Content-Type, Accept')
+            resp.body = drones[0]
+        else:
+            resp.status = falcon.HTTP_413
+            resp.body = '{"erreur": "aucun critère de recherche indiqué"}'
+
+        db.close()
+
+    def on_get(self, req, resp):
+        self.getDrone(req, resp);
+
+# instance WSGI et route vers notre API
+app = falcon.API()
+app.add_route('/drone', dgac_drone())
